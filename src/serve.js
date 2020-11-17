ed37230e845764dbd9acccfb49cfd7e9bca4a306
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-11-17 17:46:46 +0100

    add env support to serve

--- src/serve.js
+++ src/serve.js
@@ -3,6 +3,11 @@ const exec = require('./util/exec');
 
 module.exports = async (args) => {
   const port = args.p || args.port || 8000;
+  const env = [
+    `PORT=${port}`, 
+    ...args.e ? (Array.isArray(args.e) ? args.e : [args.e]) : [],
+    ...args.env ? (Array.isArray(args.env) ? args.e : [args.env]) : [],
+  ];
 
-  await exec(`PORT=${port} ts-node-dev`, 'dist/__serve');
+  await exec(`${env.join(' ')} ts-node-dev`, 'dist/__serve');
 }
